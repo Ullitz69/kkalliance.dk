7252b0f1e8deb444ede371052b4bde28b43aa289
emilu <emilub2001@gmail.com>
2026-03-24 09:41:02 +0100

    Sticky header

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ document.addEventListener("click", function () {
     menu.classList.remove("show");
   });
 });
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
